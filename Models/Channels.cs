73096a779951aba96c6274702721ede723686622
Ahmet Yavuz Işık <95169799+YavuzahmetR@users.noreply.github.com>
2023-12-10 03:34:11 +0200

    Delete Models directory

--- Models/Channels.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Layer.Core.Models
-{
-    public class Channels : BaseEntity
-    {
-        public ICollection<CampaignChannels> CampaignChannels { get; set; }
-        public string ChannelType { get; set; }//
-
-    }
-}
--- Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Layer.Core.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
