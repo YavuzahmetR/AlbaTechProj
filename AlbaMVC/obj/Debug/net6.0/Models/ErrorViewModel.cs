b23f7ae8e8fac25e12ef8e1a0f9de053b150de93
Ahmet Yavuz Işık <95169799+YavuzahmetR@users.noreply.github.com>
2026-08-15 03:28:52 +0300

    Delete AlbaMVC/obj/Debug/net6.0 directory

--- AlbaMVC/obj/Debug/net6.0/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AlbaMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
